a6570919f184c24f55d40bfadbfdd82c7524280b
Marko Budimir <marko.budimir10@gmail.com>
2022-05-03 11:15:52 +0200

    Add login button and form

--- javascript.js
+++ javascript.js
@@ -1,10 +1,24 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { myFunction() };
 
 function myFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      document.getElementById("header").className = "scrolledHeader";
-    } else {
-      document.getElementById("header").className = "";
-    }
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").className = "scrolledHeader";
+  } else {
+    document.getElementById("header").className = "";
   }
-  
+}
+
+document.getElementById("loginButton").onclick = function() {displayForm()};
+
+function displayForm() {
+  document.getElementById('id01').style.display='block';
+}
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
