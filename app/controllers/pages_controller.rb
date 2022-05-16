956a27f103b45fe90a3e881f3ef76f00ab30c088
MarkoBuda <marko.budimir10@gmail.com>
2022-05-16 15:03:10 +0200

    Converted project to ruby on rails

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+    #before_action :authenticate_user!, only: [:contacts]
+    def home
+    end
+    
+    def apartments
+    end
+
+    def location
+    end
+
+    def contacts
+    end
+end
